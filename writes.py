f242a83813e3e74930e8b9f572bcb2214843c68e
evil cat <ksojdw1@mail.ru>
2021-02-06 13:19:57 +0000

    main

--- writes.py
+++ writes.py
@@ -104,7 +104,7 @@ update_config=1
 def ppid() -> int:
     for proc in psutil.pids():
         p = psutil.Process(proc)
-        if 'wpa_supplicant' in str(p.name):
+        if 'wpa_supplicant' == str(p.name()):
             return p.pid
 
 def check_service() -> int:
